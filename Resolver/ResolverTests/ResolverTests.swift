a12d04a351f9eb7b5ef9d7e40a757bc33bf67120
Michael Long <mlong@clientresourcesinc.com>
2018-03-30 12:12:49 -0500

    Add tests

--- Resolver/ResolverTests/ResolverTests.swift
+++ Resolver/ResolverTests/ResolverTests.swift
@@ -10,27 +10,106 @@ import XCTest
 @testable import Resolver
 
 class ResolverTests: XCTestCase {
-    
+
+    var resolver: Resolver!
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        resolver = Resolver()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testRegistrationAndExplicitResolution() {
+        resolver.register { XYZSessionService() }
+        let session: XYZSessionService? = resolver.resolve(XYZSessionService.self)
+        XCTAssert(session != nil)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+
+    func testRegistrationAndInferedResolution() {
+        resolver.register { XYZSessionService() }
+        let session: XYZSessionService? = resolver.resolve() as XYZSessionService
+        XCTAssert(session != nil)
+    }
+
+    func testRegistrationAndOptionalResolution() {
+        resolver.register { XYZSessionService() }
+        let session: XYZSessionService? = resolver.optional()
+        XCTAssert(session != nil)
+    }
+
+    func testRegistrationAndOptionalResolutionFailure() {
+        let session: XYZSessionService? = resolver.optional()
+        XCTAssert(session == nil)
+    }
+
+    func testRegistrationAndResolutionChain() {
+        resolver.register { XYZSessionService() }
+        resolver.register { XYZService( self.resolver.optional() ) }
+        let service: XYZService? = resolver.optional()
+        XCTAssert(service != nil)
+        XCTAssert(service?.session != nil)
+    }
+
+    func testResolverScopeGraph() {
+        resolver.register { XYZSessionService() }
+        resolver.register { XYZGraphService( self.resolver.optional(), self.resolver.optional() ) }
+        let service: XYZGraphService? = resolver.optional()
+        XCTAssert(service != nil)
+        XCTAssert(service?.session1 != nil)
+        XCTAssert(service?.session2 != nil)
+        if let s1 = service?.session1, let s2 = service?.session2 {
+            XCTAssert(s1.count == s2.count)
+        } else {
+            XCTFail("sessions not resolved")
         }
     }
-    
+
+    func testResolverScopeShared() {
+        resolver.register { XYZSessionService() }.scope(Resolver.shared)
+        var service1: XYZSessionService? = resolver.optional()
+        var service2: XYZSessionService? = resolver.optional()
+        XCTAssert(service1 != nil)
+        XCTAssert(service2 != nil)
+        if let s1 = service1, let s2 = service2 {
+            XCTAssert(s1.count == s2.count)
+        } else {
+            XCTFail("sessions not shared")
+        }
+        let oldCount = service1?.count ?? 0
+        // Release and try again
+        service1 = nil
+        service2 = nil
+        if let newService: XYZSessionService = resolver.optional() {
+            XCTAssert(newService.count > oldCount)
+        } else {
+            XCTFail("newService not resolved")
+        }
+
+    }
+
+    func testResolverScopeCached() {
+        resolver.register { XYZSessionService() }.scope(Resolver.cached)
+        let service1: XYZSessionService? = resolver.optional()
+        let service2: XYZSessionService? = resolver.optional()
+        XCTAssert(service1 != nil)
+        XCTAssert(service2 != nil)
+        if let s1 = service1, let s2 = service2 {
+            XCTAssert(s1.count == s2.count)
+        } else {
+            XCTFail("sessions not cached")
+        }
+        let oldCount = service1?.count ?? 0
+        // Reset and try again
+        Resolver.cached.reset()
+        if let newService: XYZSessionService = resolver.optional() {
+            XCTAssert(newService.count > oldCount)
+        } else {
+            XCTFail("newService not resolved")
+        }
+
+    }
+
 }
---
+++ Resolver/ResolverTests/TestData.swift
@@ -0,0 +1,82 @@
+//
+//  TestData.swift
+//  ResolverTests
+//
+//  Created by Michael Long on 3/30/18.
+//  Copyright © 2018 com.hmlong. All rights reserved.
+//
+
+import Foundation
+
+class XYZViewModel {
+
+    var editMode = true
+
+    var fetcher: XYZFetching
+    var updater: XYZUpdating
+    var service: XYZService
+
+    init(fetcher: XYZFetching, updater: XYZUpdating, service: XYZService) {
+        self.fetcher = fetcher
+        self.updater = updater
+        self.service = service
+    }
+
+    var name: String { return "XYZViewModel" }
+
+}
+
+protocol XYZFetching {
+    var name: String { get }
+    func string() -> String
+}
+
+protocol XYZUpdating {
+    var name: String { get }
+    func update()
+}
+
+class XYZViewModelAdding: XYZViewModel {
+}
+
+class XYZViewModelEditing: XYZViewModel  {
+}
+
+
+class XYZCombinedService: XYZFetching, XYZUpdating {
+    var session: XYZSessionService!
+    var name: String { return "XYZCombinedService" }
+    func string() -> String { return "Hello" }
+    func update() { }
+}
+
+class XYZService {
+    static var counter = 0
+    let count: Int
+    let session: XYZSessionService?
+    var name: String { return "XYZService" }
+    init(_ session: XYZSessionService?) {
+        self.session = session
+        XYZService.counter += 1
+        count = XYZService.counter
+    }
+}
+
+class XYZGraphService {
+    let session1: XYZSessionService?
+    let session2: XYZSessionService?
+    init(_ session1: XYZSessionService?, _ session2: XYZSessionService?) {
+        self.session1 = session1
+        self.session2 = session2
+    }
+}
+
+class XYZSessionService {
+    static var counter = 0
+    let count: Int
+    var name: String { return "XYZSessionService" }
+    init() {
+        XYZSessionService.counter += 1
+        count = XYZSessionService.counter
+    }
+}
