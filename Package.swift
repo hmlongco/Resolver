3bf96d1084cf6fda5c7c0f677c18e87157fed8be
doozMen <stijn@dooz.be>
2020-08-08 14:04:16 +0200

    remove the need for the it to be only usable on Apple related OS's
    As the code contains enough tests for platform specific code there is no need to not build on any platform. Hence this requirement is not needed as the code is platform independend already

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Resolver",
-    platforms: [
-        .iOS(.v11),
-        .macOS(.v10_14),
-        .tvOS(.v13),
-        .watchOS(.v6)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
