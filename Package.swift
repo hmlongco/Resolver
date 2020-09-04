6dede97bcce098525fc7d5383a7f76885be2d128
Tobias Ottenweller <tobi@voiceup.chat>
2020-09-04 12:07:13 +0200

    Make swift package manager create dynamic libraries.

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Resolver",
+            type: .dynamic,
             targets: ["Resolver"]),
     ],
     dependencies: [
