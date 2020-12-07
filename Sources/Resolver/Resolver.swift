c15dcb6a826fd8ce2a36930e676ff9e888232fc9
Michael Long <mlong@clientresourcesinc.com>
2020-12-07 15:55:13 -0600

    Fix speeling error in error message ;)

--- Sources/Resolver/Resolver.swift
+++ Sources/Resolver/Resolver.swift
@@ -219,7 +219,7 @@ public final class Resolver {
             let service = registration.scope.resolve(resolver: self, registration: registration, args: args) {
             return service
         }
-        fatalError("RESOLVER: '\(Service.self):\(name ?? "")' not resolved. To disambiguate optionals use resover.optional().")
+        fatalError("RESOLVER: '\(Service.self):\(name ?? "")' not resolved. To disambiguate optionals use resolver.optional().")
     }
 
     /// Static function calls the root registry to resolve an optional Service type.
