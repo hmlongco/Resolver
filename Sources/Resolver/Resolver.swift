3d7aedc511a8c87d71d1beec70103dfe4ea0860b
Kai Maschke <kai.maschke@limeade.com>
2022-04-26 20:55:06 +0200

    Getting it fixed

--- Sources/Resolver/Resolver.swift
+++ Sources/Resolver/Resolver.swift
@@ -274,7 +274,7 @@ public final class Resolver {
         defer { lock.unlock() }
         registrationCheck()
         if let registrations = root.lookupAll(type) {
-            return registrations.compactMap { reg in return reg.scope.resolve(resolver: root, args: args) }
+            return registrations.compactMap { reg in return reg.resolve(resolver: root, args: args) }
         }
         fatalError("RESOLVER: '\(Service.self)' types not resolved.")
     }
